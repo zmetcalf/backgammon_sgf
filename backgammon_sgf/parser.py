f123471155c807158cefbe768e96b65e7b1ffc2a
Zach Metcalf <zachery.metcalf@gmail.com>
2020-07-15 20:05:47 +0000

    Started char based parsing

--- backgammon_sgf/parser.py
+++ backgammon_sgf/parser.py
@@ -10,29 +10,13 @@ class Parser:
         return Parser(data).build_model()
 
     def build_model(self):
-        self.set_matches()
-        return self.matches
+        self.clean_data()
 
-    def set_matches(self):
-        self.matches = list(
-            map(lambda element: self.parse_match(element),
-                re.findall(r'\(([^)]+)', self.data)
-            )
-        )
+        current_string = ''
+        for index, char in enumerate(self.data):
+            # TODO Handle escapes
+            pass
 
-    def parse_match(self, match_data):
-        data_and_moves = match_data.split(';')[1:]
-        return {
-            'detail': self.get_detail(data_and_moves[0:1]),
-            'moves': self.get_moves(data_and_moves[1:])
-        }
 
-    def parse_match_line(self, line):
-        pass
-
-    def get_detail(self, detail):
-        pass
-
-    def get_moves(self, moves):
-        pass
-
+    def clean_data(self):
+        self.data = self.data.replace('\n', '').replace('\r', '').replace('\t', ' ')
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -5,6 +5,17 @@ from pytest import fixture
 from backgammon_sgf.parser import Parser
 
 @fixture
+def demo_file():
+    file_path = os.path.join(
+        os.path.dirname(__file__),
+        'demo_file.sgf'
+    )
+
+    with open(file_path) as f:
+        data = f.read()
+    return data
+
+@fixture
 def gnubg_file():
     file_path = os.path.join(
         os.path.dirname(__file__),
@@ -15,12 +26,154 @@ def gnubg_file():
         data = f.read()
     return data
 
-def test_split_matches(gnubg_file):
+
+BASIC_GAME = '''
+    (;FF[4]GM[1]SZ[19]
+        ;B[aa]
+        ;W[bb]
+            (;B[cc]N[Var A];W[dd];B[ad];W[bd])
+            (;B[hh]N[Var B];W[hg])
+            (;B[gg]N[Var C];W[gh];B[hh]
+                (;W[hg]N[Var A];B[kk])
+                (;W[kl]N[Var B])
+            )
+    )
+    (;FF[4]GM[2]SZ[19]AR[\[a\]][b][c])
+'''
+
+BASIC_GAME_PARSED = [
+    {
+        'properties': {
+            'FF': '4',
+            'GM': '1',
+            'SZ': '19',
+        },
+        'nodes': [
+            {
+                'properties': {
+                    'B': 'aa'
+                }
+            },
+            {
+                'properties': {
+                    'W': 'bb'
+                },
+                'children': [
+                    {
+                        'nodes':  [
+                            {
+                                'properties': {
+                                    'B': 'cc',
+                                    'N': 'Var A',
+                                }
+                            },
+                            {
+                                'properties': {
+                                    'W': 'dd'
+                                }
+                            },
+                            {
+                                'properties': {
+                                    'B': 'ad'
+                                }
+                            },
+                            {
+                                'properties': {
+                                    'W': 'bd'
+                                }
+                            },
+                        ]
+                    },
+                    {
+                        'nodes': [
+                            {
+                                'properties': {
+                                    'B': 'hh',
+                                    'N': 'Var B'
+                                }
+                            },
+                            {
+                                'properties': {
+                                    'W': 'hg'
+                                }
+                            }
+                        ]
+                    },
+                    {
+                        'nodes': [
+                            {
+                                'properties': {
+                                    'B': 'gg',
+                                    'N': 'Var C'
+                                }
+                            },
+                            {
+                                'properties': {
+                                    'W': 'gh'
+                                }
+                            },
+                            {
+                                'properties': {
+                                    'B': 'hh'
+                                },
+                                'children': [
+                                    {
+                                        'nodes': [
+                                            {
+                                                'properties': {
+                                                    'W': 'hg',
+                                                    'N': 'Var A'
+                                                }
+                                            },
+                                            {
+                                                'properties': {
+                                                    'B': 'kk'
+                                                }
+                                            }
+                                        ]
+                                    },
+                                    {
+                                        'nodes': [
+                                            {
+                                                'properties': {
+                                                    'W': 'kl',
+                                                    'N': 'Var B'
+                                                }
+                                            }
+                                        ] # End Lvl 2 Nodes
+                                    }
+                                ] # End Lvl 2 Children
+                            }
+                        ] # End Lvl 1 Nodes
+                    }
+                ] # Endl Lvl 1 Children
+            }
+        ] # End Parent Nodes
+    }, # End First Match
+
+    # Second Match
+    {
+        'properties': {
+            'FF': '4',
+            'GM': '2',
+            'SZ': '19',
+            'AR': ['[a]', 'b', 'c']
+        }
+    }
+] # Parent Array
+
+def test_basic():
+    assert Parser.parse(BASIC_GAME) == BASIC_GAME_PARSED
+
+def test_split_matches_gnubg(gnubg_file):
     assert len(Parser.parse(gnubg_file)) == 2
+
+def test_split_matches_demo(demo_file):
+    assert len(Parser.parse(demo_file)) == 2
 
 def test_game_data_setup(gnubg_file):
     # assert Parser.parse(gnubg_file)
-    pass
+    assert 1 == 1
 
 def test_move_setup(gnubg_file):
-    pass
+    assert 1 == 1
