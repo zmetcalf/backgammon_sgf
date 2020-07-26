55aed46dfd62e0fc8893652733056d36f1875f6b
Zach Metcalf <zachery.metcalf@gmail.com>
2020-07-26 01:21:49 +0000

    Start on sgfmill

--- backgammon_sgf/parser.py
+++ backgammon_sgf/parser.py
@@ -1,4 +1,4 @@
-import re
+from sgfmill.sgf import Sgf_game
 
 class Parser:
     def __init__(self, data):
@@ -10,29 +10,5 @@ class Parser:
         return Parser(data).build_model()
 
     def build_model(self):
-        self.set_matches()
+        self.matches = Sgf_game.from_string(self.data).serialise()
         return self.matches
-
-    def set_matches(self):
-        self.matches = list(
-            map(lambda element: self.parse_match(element),
-                re.findall(r'\(([^)]+)', self.data)
-            )
-        )
-
-    def parse_match(self, match_data):
-        data_and_moves = match_data.split(';')[1:]
-        return {
-            'detail': self.get_detail(data_and_moves[0:1]),
-            'moves': self.get_moves(data_and_moves[1:])
-        }
-
-    def parse_match_line(self, line):
-        pass
-
-    def get_detail(self, detail):
-        pass
-
-    def get_moves(self, moves):
-        pass
-
